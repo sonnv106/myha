5dc1cdab28926446f2f10fc9065173c32fe31da9
sonnguyen106mh <sonnguyen106mh@gmail.com>
2024-02-27 01:17:42 +0700

    config file colors and ui auth

---
+++ src/res/colors.tsx
@@ -0,0 +1,13 @@
+const colors = {
+  primary: '#FE7940',
+  white: '#FFFFFF',
+  FE7940: '#FE7940',
+  EAEAF5: '#EAEAF5',
+  C3C2C2: '#C3C2C2',
+  A8A7A7: '#A8A7A7',
+  DED6F4: '#DED6F4',
+  E4DDF6: '#E4DDF6',
+  F0805A: '#F0805A',
+  E7E4F1: '#E7E4F1',
+}
+export default colors
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -3,21 +3,41 @@ import {
   Image,
   Pressable,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import commonStyles from '../../res/styles'
 import images from '../../res/images'
 import {Icon, Input} from '@rneui/themed'
+import colors from '../../res/colors'
 
 const LoginScreen = () => {
+  const [formType, setFormType] = useState('login')
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+  const [secureTextEntryPassword, setSecureTextEntryPassword] = useState(true)
+  const [secureTextEntryCfPassword, setSecureTextEntryCfPassword] =
+    useState(true)
+  const setFormTypeLogin = () => {
+    setFormType('login')
+  }
+  const setFormTypeSignUp = () => {
+    setFormType('signup')
+  }
+  const checkLogin = formType == 'login' ? colors.primary : colors.white
   return (
     <SafeAreaView style={commonStyles.container}>
       <StatusBar translucent />
+      {/* <ScrollView> */}
+
       <View style={[commonStyles.container, styles.container]}>
         <View style={styles.circle}>
           <Image source={images.praying} style={styles.imagePraying} />
@@ -25,47 +45,122 @@ const LoginScreen = () => {
         <View style={styles.content}>
           <View style={styles.modal}>
             <View style={[commonStyles.row, styles.viewBtn]}>
-              <Pressable style={styles.tabLogin}>
-                <Text style={styles.txtLogin}>Log In</Text>
+              <Pressable
+                style={[
+                  styles.tabLogin,
+                  {
+                    backgroundColor: checkLogin,
+                  },
+                ]}
+                onPress={setFormTypeLogin}>
+                <Text
+                  style={[
+                    styles.txtLogin,
+                    {
+                      color:
+                        formType == 'login' ? colors.white : colors.primary,
+                    },
+                  ]}>
+                  Log In
+                </Text>
               </Pressable>
-              <Pressable style={styles.btnSignUp}>
-                <Text style={styles.txtSignUp}>Sign Up</Text>
+              <Pressable
+                style={[
+                  styles.btnSignUp,
+                  {
+                    backgroundColor:
+                      formType == 'signup' ? colors.primary : colors.white,
+                  },
+                ]}
+                onPress={setFormTypeSignUp}>
+                <Text
+                  style={[
+                    styles.txtSignUp,
+                    {
+                      color: checkLogin,
+                    },
+                  ]}>
+                  Sign Up
+                </Text>
               </Pressable>
             </View>
             <View style={styles.inputContainer}>
               <Input
                 placeholder="Email"
-                placeholderTextColor={'#A8A7A7'}
-                inputContainerStyle={{
-                  borderColor: '#EAEAF5',
-                }}
+                placeholderTextColor={colors.A8A7A7}
+                inputContainerStyle={styles.inputContainerStyle}
                 inputStyle={{fontSize: 16}}
                 keyboardType="email-address"
               />
               <Input
                 placeholder="Password"
                 rightIcon={
-                  <TouchableOpacity>
-                    <Icon
-                      name="visibility"
-                      color={'#C3C2C2'}
-                      type="materialIcons"
-                      size={18}
-                    />
+                  <TouchableOpacity
+                    onPress={() => {
+                      setSecureTextEntryPassword(!secureTextEntryPassword)
+                    }}>
+                    {secureTextEntryPassword ? (
+                      <Icon
+                        name="visibility"
+                        color={colors.C3C2C2}
+                        type="materialIcons"
+                        size={18}
+                      />
+                    ) : (
+                      <Icon
+                        name="visibility-off"
+                        color={colors.C3C2C2}
+                        type="materialIcons"
+                        size={18}
+                      />
+                    )}
                   </TouchableOpacity>
                 }
-                placeholderTextColor={'#A8A7A7'}
-                inputContainerStyle={{
-                  borderColor: '#EAEAF5',
-                }}
+                placeholderTextColor={colors.A8A7A7}
+                inputContainerStyle={styles.inputContainerStyle}
                 inputStyle={{fontSize: 16}}
-                secureTextEntry
+                secureTextEntry={secureTextEntryPassword}
               />
-              <Pressable>
-                <Text style={styles.txtForgotPassword}>Forgot password?</Text>
-              </Pressable>
+              {formType == 'signup' ? (
+                <Input
+                  placeholder="Confirm password"
+                  rightIcon={
+                    <TouchableOpacity
+                      onPress={() => {
+                        setSecureTextEntryCfPassword(!secureTextEntryCfPassword)
+                      }}>
+                      {secureTextEntryCfPassword ? (
+                        <Icon
+                          name="visibility"
+                          color={colors.C3C2C2}
+                          type="materialIcons"
+                          size={18}
+                        />
+                      ) : (
+                        <Icon
+                          name="visibility-off"
+                          color={colors.C3C2C2}
+                          type="materialIcons"
+                          size={18}
+                        />
+                      )}
+                    </TouchableOpacity>
+                  }
+                  placeholderTextColor={colors.A8A7A7}
+                  inputContainerStyle={styles.inputContainerStyle}
+                  inputStyle={{fontSize: 16}}
+                  secureTextEntry={secureTextEntryCfPassword}
+                />
+              ) : null}
+              {formType == 'login' ? (
+                <Pressable>
+                  <Text style={styles.txtForgotPassword}>Forgot password?</Text>
+                </Pressable>
+              ) : null}
               <TouchableOpacity style={styles.btnLogin}>
-                <Text style={styles.txtLogIn}>Log In</Text>
+                <Text style={styles.txtLogIn}>
+                  {formType == 'login' ? `Log In` : `Sign Up`}
+                </Text>
               </TouchableOpacity>
               <Text style={styles.txtOr}>OR</Text>
               <View style={styles.vGoogleFB}>
@@ -80,6 +175,7 @@ const LoginScreen = () => {
           </View>
         </View>
       </View>
+      {/* </ScrollView> */}
     </SafeAreaView>
   )
 }
@@ -90,6 +186,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: 'relative',
+    paddingBottom: 200,
+    marginBottom: 200,
   },
   imagePraying: {
     width: Dimensions.get('window').width,
@@ -100,21 +198,22 @@ const styles = StyleSheet.create({
     height: 350,
   },
   content: {
+    flex: 1,
     padding: 20,
     position: 'absolute',
     width: '100%',
-    top: 160,
+    top: 120,
   },
   modal: {
     borderRadius: 43,
     elevation: 20,
     borderWidth: 0.5,
-    borderColor: '#E7E4F1',
+    borderColor: colors.E7E4F1,
     width: '100%',
-    backgroundColor: '#FFF',
+    backgroundColor: colors.white,
     padding: 20,
     paddingTop: 50,
-    shadowColor: '#DED6F4',
+    shadowColor: colors.DED6F4,
     shadowOffset: {
       width: 10,
       height: 10,
@@ -123,24 +222,25 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
   },
   viewBtn: {
-    shadowColor: '#DED6F4',
+    shadowColor: colors.DED6F4,
     shadowOffset: {
       width: 0,
       height: 10,
     },
     shadowOpacity: 0.5,
     shadowRadius: 2.22,
-    backgroundColor: '#FFF',
+    elevation: 5,
+    backgroundColor: colors.white,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#EAEAF5',
+    borderColor: colors.EAEAF5,
   },
   tabLogin: {
     width: '50%',
     justifyContent: 'center',
     alignItems: 'center',
     height: 45,
-    backgroundColor: '#FE7940',
+    backgroundColor: colors.primary,
     borderRadius: 10,
   },
   btnSignUp: {
@@ -148,20 +248,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     height: 45,
-    backgroundColor: '#FFF',
+    backgroundColor: colors.white,
     borderRadius: 10,
     overflow: 'hidden',
   },
   txtLogin: {
-    color: '#FFF',
+    color: colors.white,
   },
   txtSignUp: {
-    color: '#FE7940',
+    color: colors.primary,
     fontWeight: '600',
   },
   circle: {
     borderRadius: 500,
-    backgroundColor: '#F0805A',
+    backgroundColor: colors.F0805A,
     height: 517,
     width: 517,
     position: 'absolute',
@@ -175,25 +275,34 @@ const styles = StyleSheet.create({
   txtForgotPassword: {
     textAlign: 'right',
     marginRight: 10,
-    color: '#A8A7A7',
+    color: colors.A8A7A7,
   },
   btnLogin: {
+    shadowColor: colors.E4DDF6,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.7,
+    shadowRadius: 2.22,
+    elevation: 5,
     width: '100%',
     height: 45,
-    backgroundColor: '#FE7940',
+    backgroundColor: colors.primary,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 23,
     marginTop: 54,
   },
   txtLogIn: {
-    color: '#FFFFFF',
+    color: colors.white,
     fontWeight: 'bold',
+    fontSize: 16,
   },
   txtOr: {
     textAlign: 'center',
     marginTop: 28,
-    color: '#A8A7A7',
+    color: colors.A8A7A7,
   },
   vGoogleFB: {
     flexDirection: 'row',
@@ -205,10 +314,13 @@ const styles = StyleSheet.create({
     width: 48,
     height: 48,
     borderRadius: 24,
-    borderColor: '#EAEAF5',
+    borderColor: colors.EAEAF5,
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
     marginHorizontal: 10,
+  },
+  inputContainerStyle: {
+    borderColor: colors.EAEAF5,
   },
 })
